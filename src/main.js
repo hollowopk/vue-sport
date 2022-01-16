d9ac0bb8b353f74812673031d1884e1f8a5c3d64
吕国伟 <lvguowei2024@163.com>
2022-01-16 11:49:56 +0000

    update src/main.js. 引入element ui

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import store from './store'
 import './plugins/element.js'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
