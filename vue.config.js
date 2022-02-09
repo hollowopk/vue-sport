66bde62d840f0ad8c69dedb2c4d0d3a1d99c2d4e
吕国伟 <lvguowei2024@163.com>
2022-02-09 10:28:08 +0000

    update vue.config.js. 8000

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     devServer:{
         proxy:{
             "/api":{
-                target:"http://localhost:8080/",
+                target:"http://localhost:8000/",
                 pathRewrite:{ // 重写路径
                     "^/api":""
                 },
